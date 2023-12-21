b4d5fe6efd2ad4bddc3982736d322148db557cae
Damian Poddebniak <poddebniak@mailbox.org>
2023-12-21 10:37:21 +0100

    chore: Reformat code
    Co-authored-by: Jakob Schikowski <jakob.schikowski@gmx.de>

--- src/client.rs
+++ src/client.rs
@@ -268,9 +268,13 @@ pub enum ClientFlowEvent {
         status: Status<'static>,
     },
     /// Server [`Data`] received.
-    DataReceived { data: Data<'static> },
+    DataReceived {
+        data: Data<'static>,
+    },
     /// Server [`Status`] received.
-    StatusReceived { status: Status<'static> },
+    StatusReceived {
+        status: Status<'static>,
+    },
     /// Server [`CommandContinuationRequest`] response received.
     ///
     /// Note: The received continuation was not part of [`ClientFlow`] literal handling.
