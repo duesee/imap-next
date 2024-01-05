236be5d36c993015a1b7e9152203ff90d17e6023
Damian Poddebniak <poddebniak@mailbox.org>
2024-01-05 17:08:22 +0100

    chore: Improve `server` example

--- examples/server.rs
+++ examples/server.rs
@@ -7,21 +7,21 @@ use tokio::net::TcpListener;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let stream = {
-        let listener = TcpListener::bind("127.0.0.1:12345").await.unwrap();
+    let (mut server, _) = {
+        let stream = {
+            let listener = TcpListener::bind("127.0.0.1:12345").await.unwrap();
+            let (stream, _) = listener.accept().await.unwrap();
+            stream
+        };
 
-        let (stream, _) = listener.accept().await.unwrap();
-
-        stream
+        ServerFlow::send_greeting(
+            AnyStream::new(stream),
+            ServerFlowOptions::default(),
+            Greeting::ok(None, "server (example)").unwrap(),
+        )
+        .await
+        .unwrap()
     };
-
-    let (mut server, _) = ServerFlow::send_greeting(
-        AnyStream::new(stream),
-        ServerFlowOptions::default(),
-        Greeting::ok(None, "Hello, World!").unwrap(),
-    )
-    .await
-    .unwrap();
 
     let mut handle = None;
 
