5c76e9898c845b13a01bc4237db499d5f95e0e96
Damian Poddebniak <poddebniak@mailbox.org>
2024-01-08 14:48:07 +0100

    feat: Implement `From<CommandAuthenticate>` for `Command`

--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,27 @@
 use std::borrow::Cow;
 
-use imap_codec::imap_types::{auth::AuthMechanism, core::Tag, secret::Secret};
+use imap_codec::imap_types::{
+    auth::AuthMechanism,
+    command::{Command, CommandBody},
+    core::Tag,
+    secret::Secret,
+};
 
 #[derive(Debug)]
 pub struct CommandAuthenticate {
     pub tag: Tag<'static>,
     pub mechanism: AuthMechanism<'static>,
     pub initial_response: Option<Secret<Cow<'static, [u8]>>>,
+}
+
+impl From<CommandAuthenticate> for Command<'static> {
+    fn from(command_authenticate: CommandAuthenticate) -> Self {
+        Self {
+            tag: command_authenticate.tag,
+            body: CommandBody::Authenticate {
+                mechanism: command_authenticate.mechanism,
+                initial_response: command_authenticate.initial_response,
+            },
+        }
+    }
 }
