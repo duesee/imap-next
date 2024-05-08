0bc34c7eabcfc73a0876b37750d26af426074154
Clément DOUIN <clement.douin@posteo.net>
2024-05-08 10:48:10 +0200

    simplify and comment flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
+    # The rustup equivalent for Nix.
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Allows non-flakes users to still be able to `nix-shell` based on
+    # `shell.nix` instead of this `flake.nix`.
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -17,6 +20,7 @@
     let
       inherit (nixpkgs) lib;
 
+      eachSupportedSystem = lib.genAttrs supportedSystems;
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -24,32 +28,22 @@
         "aarch64-darwin"
       ];
 
-      eachSupportedSystem = lib.genAttrs supportedSystems;
-
       mkDevShells = system:
         let
           pkgs = import nixpkgs { inherit system; };
 
+          # get the rust toolchain from the rustup
+          # `rust-toolchain.toml` configuration file
           rust-toolchain = fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
             sha256 = "opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
           };
 
-          defaultShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              # Nix
-              nixd
-              nixpkgs-fmt
-
-              # Rust
-              rust-toolchain
-              cargo-watch
-            ];
-          };
-
         in
         {
-          default = defaultShell;
+          default = pkgs.mkShell {
+            buildInputs = [ rust-toolchain ];
+          };
         };
 
     in
