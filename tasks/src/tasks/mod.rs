8a3cd9bb52069d0c8dcd8f4b2052b841f064d268
Clément DOUIN <clement.douin@posteo.net>
2024-05-12 19:39:20 +0200

    feat: add `NoOpTask`

--- tasks/src/tasks/mod.rs
+++ tasks/src/tasks/mod.rs
@@ -1,3 +1,16 @@
+use imap_types::response::StatusBody;
+use thiserror::Error;
+
 pub mod authenticate;
 pub mod capability;
 pub mod logout;
+pub mod noop;
+
+#[derive(Debug, Error)]
+pub enum TaskError {
+    #[error("unexpected BAD response: {}", .0.text)]
+    UnexpectedBadResponse(StatusBody<'static>),
+
+    #[error("unexpected NO response: {}", .0.text)]
+    UnexpectedNoResponse(StatusBody<'static>),
+}
---
+++ tasks/src/tasks/noop.rs
@@ -0,0 +1,31 @@
+use imap_types::{
+    command::CommandBody,
+    response::{StatusBody, StatusKind},
+};
+
+use crate::{tasks::TaskError, Task};
+
+#[derive(Clone, Debug, Default)]
+pub struct NoOpTask;
+
+impl NoOpTask {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl Task for NoOpTask {
+    type Output = Result<(), TaskError>;
+
+    fn command_body(&self) -> CommandBody<'static> {
+        CommandBody::Noop
+    }
+
+    fn process_tagged(self, status_body: StatusBody<'static>) -> Self::Output {
+        match status_body.kind {
+            StatusKind::Ok => Ok(()),
+            StatusKind::No => Err(TaskError::UnexpectedNoResponse(status_body)),
+            StatusKind::Bad => Err(TaskError::UnexpectedBadResponse(status_body)),
+        }
+    }
+}
