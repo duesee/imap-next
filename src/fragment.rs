8789f109caceb04bc9352def6c89aac29b43c212
Jakob Schikowski <jakob.schikowski@posteo.de>
2025-11-15 20:38:57 +0100

    chore: Improve FragmentKind

--- src/fragment.rs
+++ src/fragment.rs
@@ -4,17 +4,18 @@ use imap_codec::{
 };
 use tracing::{instrument, trace};
 
+/// Describes the type of a fragment for logging purposes.
 #[derive(Debug)]
 pub enum FragmentKind {
     Line,
     Literal,
 }
 
-impl FragmentKind {
-    fn from_info(frament_info: FragmentInfo) -> Self {
+impl From<FragmentInfo> for FragmentKind {
+    fn from(frament_info: FragmentInfo) -> Self {
         match frament_info {
-            FragmentInfo::Line { .. } => FragmentKind::Line,
-            FragmentInfo::Literal { .. } => FragmentKind::Literal,
+            FragmentInfo::Line { .. } => Self::Line,
+            FragmentInfo::Literal { .. } => Self::Literal,
         }
     }
 }
@@ -24,7 +25,7 @@ pub fn read_fragment(fragmentizer: &mut Fragmentizer) -> Option<FragmentInfo> {
     let fragment_info = fragmentizer.progress()?;
 
     trace!(
-        type = ?FragmentKind::from_info(fragment_info),
+        type = ?FragmentKind::from(fragment_info),
         data = escape_byte_string(fragmentizer.fragment_bytes(fragment_info)),
         complete = fragmentizer.is_message_complete(),
         exceeded = fragmentizer.is_max_message_size_exceeded(),
